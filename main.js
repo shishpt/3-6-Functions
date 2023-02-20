696e218d78198fcb033e3d27b09c92d8ad9deb02
shishir <shishirpatel@gmail.com>
2023-02-20 13:25:22 +0000

    addition of js file

---
+++ main.js
@@ -0,0 +1,13 @@
+//Contact list
+//contact();
+// Create a contact list
+const contact = [
+  { firstName: 'David', lastName: 'Attenborough', email: 'name@test.com', phone: '555-5555', company: 'Testers Chemical Plant' },
+  { firstName: 'Jennifer', lastName: 'Abbott', email: 'testers@fakecoffee.com', phone: '555-5454', company: 'Testers Coffee Shop' },
+  { firstName: 'David', lastName: 'Attenborough', email: 'testers@autogroup.com', phone: '555-5353', company: 'Testers Auto Group' }
+];
+
+// Add contact 
+contact.push('Benjamin', 'Smith', 'smith@fakemail.edu', '555-5959', 'Fake University');
+
+console.log(contact);
